829c4ccc9b92428ff8aa6b04073159dd97c2ebd2
Nishith Nishith <lnu.nish@northeastern.edu>
2023-10-04 06:32:15 -0400

    Bootstap bug fix

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 import express from "express";
+import { Sequelize } from "sequelize";
 import { sequelize } from "./services/database/db.js";
 import route from './routes/index.js'
 import { user_add, user_details } from "./services/database/bootstrap.js";
+import Account from "./models/Account.js";
+import Assignment from "./models/Assignment.js";
+import AccountAssignmentMap from "./models/Account-Assignment-Map.js";
 
 //Bootstrap
 try{
-  sequelize.sync();
+  let sq_call = async()=>{
+    await sequelize.sync();
+  }
+  await sq_call()
   user_details();
 }
 catch(err){
