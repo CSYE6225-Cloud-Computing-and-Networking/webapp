c6122f92c79dffab502efe6f85a88bc0d58bb5d1
Nishith Nishith <lnu.nish@northeastern.edu>
2023-12-06 13:51:28 -0500

    demo

--- server/routes/assignment-route.js
+++ server/routes/assignment-route.js
@@ -3,7 +3,7 @@ const router = express.Router()
 import * as assignmentController from '../controller/assignment-controller.js'
 import { verifyToken } from '../services/util/verify-account.js';
 
-router.get('/v3/assignment', verifyToken, assignmentController.list);
+router.get('/demo/assignment', verifyToken, assignmentController.list);
 router.get('/v1/assignment/:id', verifyToken, assignmentController.get )
 
 router.post('/v1/assignment', verifyToken, assignmentController.create);
