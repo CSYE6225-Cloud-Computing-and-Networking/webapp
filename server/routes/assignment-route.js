b423a1a5c0d6710193467785eb72876bc105eb0a
Nishith Nishith <lnu.nish@northeastern.edu>
2023-12-06 15:10:05 -0500

    demo2

--- server/routes/assignment-route.js
+++ server/routes/assignment-route.js
@@ -3,21 +3,21 @@ const router = express.Router()
 import * as assignmentController from '../controller/assignment-controller.js'
 import { verifyToken } from '../services/util/verify-account.js';
 
-router.get('/demo/assignment', verifyToken, assignmentController.list);
-router.get('/v1/assignment/:id', verifyToken, assignmentController.get )
+router.get('/v3/assignment', verifyToken, assignmentController.list);
+router.get('/v3/assignment/:id', verifyToken, assignmentController.get )
 
-router.post('/v1/assignment', verifyToken, assignmentController.create);
-router.put('/v1/assignment/:id', verifyToken, assignmentController.update)
-router.delete('/v1/assignment/:id', verifyToken, assignmentController.del)
-router.post('/v1/assignment/:id/submission', verifyToken, assignmentController.submission)
+router.post('/v3/assignment', verifyToken, assignmentController.create);
+router.put('/v3/assignment/:id', verifyToken, assignmentController.update)
+router.delete('/v3/assignment/:id', verifyToken, assignmentController.del)
+router.post('/v3/assignment/:id/submission', verifyToken, assignmentController.submission)
 
-router.all('/v1/assignment',(req,res)=>{
+router.all('/v3/assignment',(req,res)=>{
     res.set('Cache-control', `no-store`)
     res.status(405)
     res.send()
 })
 
-router.all('/v1/assignment/:id',(req,res)=>{
+router.all('/v3/assignment/:id',(req,res)=>{
     res.set('Cache-control', `no-store`)
     res.status(405)
     res.send()
